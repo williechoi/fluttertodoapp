7fc4768dde2bdb2059aaabaa35c1663e940b385c
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-07-10 22:59:44 +0900

    todo app

---
+++ android/app/src/main/kotlin/choi/example/todoapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package choi.example.todoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Todo {
+  bool isDone = false;
+  String title;
+
+  Todo(this.title);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo Management',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final _items = <Todo>[];
+
+  var _todoController = TextEditingController();
+
+  @override
+  void dispose() {
+    _todoController.dispose();
+    super.dispose();
+  }
+
+  void _addTodo(Todo todo) {
+    setState(() {
+      _items.add(todo);
+      _todoController.text = '';
+    });
+  }
+
+  void _deleteTodo(Todo todo) {
+    setState(() {
+      _items.remove(todo);
+    });
+  }
+
+  void _toggleTodo(Todo todo) {
+    setState(() {
+      todo.isDone = ! todo.isDone;
+    });
+  }
+
+  Widget _buildItemWidget(Todo todo) {
+    return ListTile(
+        onTap: () {},
+        title: Text(
+            todo.title,
+            style: todo.isDone ? const TextStyle(
+              decoration: TextDecoration.lineThrough,
+              fontStyle: FontStyle.italic,
+            ) : null,
+        ),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete_forever),
+        onPressed: () => _deleteTodo(todo),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Things to do'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _todoController,
+                  ),
+                ),
+                ElevatedButton(
+                  child: const Text('Add'),
+                  onPressed: () => _addTodo(Todo(_todoController.text)),
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView(
+                children: _items.map((todo) => _buildItemWidget(todo)).toList(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
